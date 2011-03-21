f5b2a721ca743591aedb62d5d2a633086972f3fa
George Lesica <glesica@gmail.com>
2011-03-21 02:23:03 -0500

    Started implementing build script with jinja2.

--- src/build.py
+++ src/build.py
@@ -1,11 +1,33 @@
 import sys, os
+from jinja2 import Environment, PackageLoader
 
-def getSection(name):
-    try:
-        sfile = open(os.path.join('src', name + '.html'), 'r')
-        return sfile.readlines()
-    except IOError:
-        return ''
+class LayoutBuilder(object):
+    '''
+    Handles building a full layout
+    '''
+    
+    def __init__(self):
+        self.template_environment = Environment(
+            loader=PackageLoader('nice', 'templates')
+        )
+
+    def getSection(name):
+        template_file = name
+        if not name.startswith('section_'):
+            template = 'section_' + template_file
+        if not name.endswith('.html'):
+            template_file = template_file + '.html'
+    
+        t = template_environment.get_template(template_file)
+        return t.render()
+    
+    def buildLayout(sections):
+        context = {}
+        
+        for section in sections:
+            context[section] = getSection(section)
+        
+        return 
 
 def main(argv):
     
